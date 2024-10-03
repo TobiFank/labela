eeb87795fd729e1cc066bee4d45536b8d82e951c
TobiFank <tobias7402@gmail.com>
2024-10-03 09:17:28 +0200

    update windows script

--- start_windows.bat
+++ start_windows.bat
@@ -1,6 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:: Change to the directory where the script is located
+cd /d "%~dp0"
+
 echo Starting Image Captioner setup...
 echo Current directory: %cd%
 
@@ -15,9 +18,7 @@ if not exist Dockerfile (
 :: Check if Docker is installed
 echo Checking if Docker is installed...
 where docker >nul 2>&1
-if %errorlevel% equ 0 (
-    echo Docker is installed.
-) else (
+if %errorlevel% neq 0 (
     echo Docker is not found in PATH. Please install Docker Desktop for Windows and run this script again.
     goto :end
 )
@@ -28,7 +29,6 @@ docker info >nul 2>&1
 if %errorlevel% neq 0 (
     echo Docker is not running. Attempting to start Docker...
     start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-
     echo Waiting for Docker to start...
     :docker_wait_loop
     timeout /t 5 >nul
@@ -38,78 +38,19 @@ if %errorlevel% neq 0 (
         goto docker_wait_loop
     )
 )
-
 echo Docker is running.
 
 :: Create host directories if they don't exist
 if not exist .\example_images mkdir .\example_images
 if not exist .\images_to_caption mkdir .\images_to_caption
-
 echo Created necessary directories.
 
-:: Function to get the latest modification time of files in a directory
-:get_latest_mtime
-set "latest_time=0"
-set "latest_file="
-for /r "%~1" %%F in (*) do (
-    for /f "tokens=2 delims==" %%T in ('wmic datafile where name="%%~sF" get LastModified /value') do (
-        if %%T gtr !latest_time! (
-            set "latest_time=%%T"
-            set "latest_file=%%F"
-        )
-    )
-)
-exit /b
-
-:: Check if the image exists and rebuild if necessary
-docker image inspect image-captioner >nul 2>&1
-if %errorlevel% equ 0 (
-    echo Docker image exists. Checking for updates...
-
-    :: Get the creation time of the Docker image
-    for /f "tokens=2 delims=: " %%a in ('docker inspect -f "{{.Created}}" image-captioner') do set IMAGE_CREATED=%%a
-    echo Image creation time: %IMAGE_CREATED%
-
-    :: Get the latest modification time of Dockerfile
-    for /f "tokens=2 delims==" %%a in ('wmic datafile where name="%cd:\=\\%\\Dockerfile" get LastModified /value') do set DOCKERFILE_MODIFIED=%%a
-    echo Dockerfile last modified: %DOCKERFILE_MODIFIED%
-
-    :: Get the latest modification time of files in the current directory
-    call :get_latest_mtime "%cd%"
-    echo Latest app file modified: %latest_file%
-    echo Latest app file modification time: %latest_time%
-
-    :: Check requirements.txt if it exists
-    if exist requirements.txt (
-        for /f "tokens=2 delims==" %%a in ('wmic datafile where name="%cd:\=\\%\\requirements.txt" get LastModified /value') do set REQUIREMENTS_MODIFIED=%%a
-        echo Requirements file last modified: %REQUIREMENTS_MODIFIED%
-    ) else (
-        set "REQUIREMENTS_MODIFIED=0"
-        echo No requirements.txt file found
-    )
-
-    :: Compare modification times
-    set "LATEST_MODIFIED=%DOCKERFILE_MODIFIED%"
-    if %latest_time% gtr !LATEST_MODIFIED! set "LATEST_MODIFIED=!latest_time!"
-    if %REQUIREMENTS_MODIFIED% gtr !LATEST_MODIFIED! set "LATEST_MODIFIED=%REQUIREMENTS_MODIFIED%"
-
-    echo Most recent modification: %LATEST_MODIFIED%
-
-    if !LATEST_MODIFIED! gtr !IMAGE_CREATED! (
-        echo Changes detected. Rebuilding the image...
-        docker build -t image-captioner . || (
-            echo Failed to build Docker image. Please check the Dockerfile and try again.
-            goto :end
-        )
-    ) else (
-        echo Docker image is up-to-date. No rebuild necessary.
-    )
-) else (
-    echo Docker image doesn't exist. Building the image...
-    docker build -t image-captioner . || (
-        echo Failed to build Docker image. Please check the Dockerfile and try again.
-        goto :end
-    )
+:: Always rebuild the Docker image
+echo Rebuilding the Docker image...
+docker build -t image-captioner .
+if %errorlevel% neq 0 (
+    echo Failed to build Docker image. Please check the Dockerfile and try again.
+    goto :end
 )
 
 :: Stop and remove the existing container if it's running
